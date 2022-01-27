eae7713bf8511b59d4468e2e3864ac70ab1a7c64
Christian Boethius <christian@boethius.ch>
2022-01-27 16:59:12 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,34 @@
+
+const ani = gsap.timeline()
+
+ani.to('.animation img', {
+scale: 1.4,
+duration:0.6,
+ease:'linear'
+})
+.to('.animation img',{
+scale: 1,
+duration:0.6,
+ease:'bounce.out'
+})
+.to('.animation img',{
+scale: 1.4,
+duration:0.6,
+ease:'linear'
+})
+.to('.animation img',{
+scale: 1,
+duration:0.6,
+ease:'bounce.out'
+})
+.to('.animationg img',{
+scale: 1.4,
+duration:0.6,
+ease:'linear'
+})
+.to('.animation, .animationg img ',{
+scale: 1,
+duration:0.1,
+ease:'bounce.out',
+autoAlpha: 0,
+})
